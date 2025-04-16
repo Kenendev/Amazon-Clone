0cb2ada18ca723351052690ecf00dab92cb4b2d3
Lift-Dev <oneofii1122@gmail.com>
2025-04-16 20:51:14 +0300

    Add project files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      hello world!
+     </div>
+     <div></div>
+     <div></div>
+
+    </div>
+  );
+}
+
+export default App;
